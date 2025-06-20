40306fb9ac51d0a271329ccc97e3ccd4aaf34461
Sean Murphy <sean@iamseanmurphy.com>
2025-06-20 10:18:38 +0000

    2.0.3

--- hyperlink-group-block/build/index.asset.php
+++ hyperlink-group-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '14318ae07301f867b0b3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '6615d23d56e9419fc330');
